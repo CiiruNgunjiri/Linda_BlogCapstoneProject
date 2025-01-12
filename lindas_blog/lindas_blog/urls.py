a9b1e3a227b8bf4e4d1e702d8f560429fc3980e8
Linda Ngunjiri <ciiru.ngunjiri@gmail.com>
2025-01-12 09:29:10 +0300

    modified root urls.py file

--- lindas_blog/lindas_blog/urls.py
+++ lindas_blog/lindas_blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import handler404, handler500
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Admin site
@@ -9,6 +10,7 @@ urlpatterns = [
     # Include blog app URLs for both frontend and API
     path('api/', include('blog.urls')),  # API URLs are now included from blog/urls.py
 
+    path('login/', auth_views.LoginView.as_view(), name='login'),  # Define the 'login' view here
     # Optional: Custom error handling views can be added here if needed
 ]
 
